9e51b76e41bef03bbc0099f9139419fa6a95a2d1
Zack Cerza <zack@redhat.com>
2023-02-21 16:43:01 -0700

    f e beanstalk

--- teuthology/exporter.py
+++ teuthology/exporter.py
@@ -3,6 +3,8 @@ import time
 
 from prometheus_client import start_http_server, Gauge
 
+from teuthology.beanstalk import connect, stats_tube, watch_tube
+from teuthology.config import config
 from teuthology.dispatcher import find_dispatcher_processes
 
 
@@ -20,10 +22,22 @@ class TeuthologyMetrics:
         self.beanstalk_queue_length = Gauge(
             "beanstalk_queue_length", "Beanstalk Queue Length", ["machine_type"]
         )
+        self.beanstalk_queue_paused = Gauge(
+            "beanstalk_queue_paused", "Beanstalk Queue is Paused", ["machine_type"]
+        )
 
     def update(self):
-        for machine_type, procs in find_dispatcher_processes().items():
-            self.dispatcher_count.labels(machine_type).set(len(procs))
+        machine_types = list(config.active_machine_types)
+        dispatcher_procs = find_dispatcher_processes()
+        for machine_type in machine_types:
+            self.dispatcher_count.labels(machine_type).set(
+                len(dispatcher_procs.get(machine_type, []))
+            )
+            queue_stats = stats_tube(connect(), machine_type)
+            self.beanstalk_queue_length.labels(machine_type).set(queue_stats["count"])
+            self.beanstalk_queue_paused.labels(machine_type).set(
+                1 if queue_stats["paused"] else 0
+            )
 
     def loop(self):
         log.info("Starting teuthology-exporter...")
